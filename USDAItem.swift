bddfeb8013058bbe6ef61e8909e0faad7d1be493
Mark Rabins <markrabins@gmail.com>
2015-10-31 22:18:14 -0700

    Implement CoreData

---
+++ USDAItem.swift
@@ -0,0 +1,27 @@
+//
+//  USDAItem.swift
+//  FoodTracker
+//
+//  Created by Mark Rabins on 10/31/15.
+//  Copyright © 2015 self.swift. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+@objc(USDAItem)
+class USDAItem: NSManagedObject {
+
+    @NSManaged var calcuim: String
+    @NSManaged var carbohydrate: String
+    @NSManaged var cholesterol: String
+    @NSManaged var dateAdded: NSDate
+    @NSManaged var energy: String
+    @NSManaged var fatTotal: String
+    @NSManaged var idValue: String
+    @NSManaged var name: String
+    @NSManaged var protein: String
+    @NSManaged var sugar: String
+    @NSManaged var vitaminC: String
+}
