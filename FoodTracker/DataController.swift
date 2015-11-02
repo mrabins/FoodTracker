3cb612230fb4b6d12e36075ce556aa9acb30b193
Mark Rabins <markrabins@gmail.com>
2015-11-01 22:49:37 -0800

    Further CoreData processing and saving

--- FoodTracker/DataController.swift
+++ FoodTracker/DataController.swift
@@ -105,6 +105,61 @@ class DataController {
                                     else {
                                         usdaItem.fatTotal = "0"
                                     }
+
+                                    if usdaFieldsDictionary["CHOLE"] != nil {
+                                        let cholesterolDictionary = usdaFieldsDictionary["CHOLE"]! as NSDictionary
+                                        if cholesterolDictionary["value"] != nil {
+                                            let cholesterolValue: AnyObject = cholesterolDictionary["value"]!
+                                            usdaItem.cholesterol = "\(cholesterolValue)"
+                                        }
+                                    }
+                                    else {
+                                        usdaItem.cholesterol = "0"
+                                    }
+
+                                    if usdaFieldsDictionary["PROCNT"] != nil {
+                                        let proteinDictionary = usdaFieldsDictionary["PROCNT"]! as NSDictionary
+                                        if proteinDictionary ["value"] != nil {
+                                            let proteinValue: AnyObject = proteinDictionary ["value"]!
+                                            usdaItem.protein = "\(proteinValue)"
+                                            }
+                                       
+                                        }
+                                        else {
+                                          usdaItem.protein = "0"
+                                    }
+                                    
+                                    if usdaFieldsDictionary["SUGAR"] != nil {
+                                        let sugarDictionary = usdaFieldsDictionary["SUGAR"]! as NSDictionary
+                                        if sugarDictionary["value"] != nil {
+                                            let sugarValue: AnyObject = sugarDictionary["value"]!
+                                            usdaItem.sugar = "\(sugarValue)"
+                                        }
+                                    }
+                                    else {
+                                        usdaItem.sugar = "0"
+                                    }
+                                    if usdaFieldsDictionary["VITC"] != nil {
+                                        let vitaminCDictionary = usdaFieldsDictionary["VITC"]! as NSDictionary
+                                        if vitaminCDictionary["value"] != nil {
+                                            let vitaminCValue: AnyObject = vitaminCDictionary["value"]!
+                                            usdaItem.vitaminC = "\(vitaminCValue)"
+                                        }
+                                    }
+                                    else {
+                                        usdaItem.vitaminC = "0"
+                                    }
+                                    if usdaFieldsDictionary["ENERC_KCAL"] != nil {
+                                        let energyDictionary = usdaFieldsDictionary["ENERC_KCAL"]! as NSDictionary
+                                        if energyDictionary["value"] != nil {
+                                            let energyValue: AnyObject = energyDictionary["value"]!
+                                            usdaItem.energy = "\(energyValue)"
+                                        }
+                                    }
+                                    else {
+                                        usdaItem.energy = "0"
+                                    }
+                                    (UIApplication.sharedApplication().delegate as AppDelegate).saveContext()
                                 }
                             }
                             
