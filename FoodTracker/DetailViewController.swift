fc57f6da40f2ac797ceb0f8e3501d0e1db349554
Mark Rabins <markrabins@gmail.com>
2015-11-08 23:35:57 -0800

    UI Attribute String updates to DetailViewController

--- FoodTracker/DetailViewController.swift
+++ FoodTracker/DetailViewController.swift
@@ -24,6 +24,10 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        if usdaItem != nil {
+            textView.attributedText = createAttributedString(usdaItem!)
+        }
     }
     
     deinit {
@@ -39,9 +43,50 @@ class DetailViewController: UIViewController {
         
         println("usdaItemDidComplete in DetailViewController")
         usdaItem = notification.object as? USDAItem
+        
+        if self.isViewLoaded() && self.view.window != nil {
+            textView.attributedText = createAttributedString(usdaItem!)
+        }
     }
 
     @IBAction func eatItButtonPressed(sender: UIBarButtonItem) {
         
+    }
+    
+    func createAttributedString (usdaItem: USDAItem) -> NSAttributedString {
+        
+        var itemAttributedString = NSMutableAttributedString()
+        var centeredParagraphStyling = NSMutableParagraphStyle()
+        centeredParagraphStyling.alignment = NSTextAlignment.Center
+        centeredParagraphStyling.lineSpacing = 10.0
+        
+        var titleAttributedDictionary = [
+            NSForegroundColorAttributeName : UIColor.blackColor(),
+            NSFontAttributeName : UIFont.boldSystemFontOfSize(22.0),
+            NSParagraphStyleAttributeName : centeredParagraphStyling]
+        
+        let titleString = NSAttributedString(string: "\(usdaItem.name)\n", attributes: titleAttributedDictionary)
+        itemAttributedString.appendAttributedString(titleString)
+        
+        var leftAlignedParagraphStyle = NSMutableParagraphStyle()
+            leftAlignedParagraphStyle.alignment = NSTextAlignment.Left
+            leftAlignedParagraphStyle.lineSpacing = 20.0
+        
+        var styleFirstWordAttributesDictionary = [
+            NSForegroundColorAttributeName : UIColor.blackColor(),
+            NSFontAttributeName : UIFont.boldSystemFontOfSize(18.0),
+            NSParagraphStyleAttributeName : leftAlignedParagraphStyle]
+        
+        var style1AttributesDictionary = [
+            NSForegroundColorAttributeName : UIColor.darkGrayColor(),
+            NSFontAttributeName : UIFont.systemFontOfSize(18.0),
+            NSParagraphStyleAttributeName : leftAlignedParagraphStyle]
+        
+        var style2AttributesDictionary = [
+            NSForegroundColorAttributeName : UIColor.lightGrayColor(),
+            NSFontAttributeName : UIFont.systemFontOfSize(18.0),
+            NSParagraphStyleAttributeName : leftAlignedParagraphStyle]
+        
+        return itemAttributedString
     }
 }
